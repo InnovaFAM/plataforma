2c65907e190bb552d7c78a029e3813644225682f
Patricio Cornejo <soporte@innovafam.cl>
2026-05-12 14:46:09 -0400

    aws account number changed

--- backend/lambdas/v1-services-ai/src/constants.py
+++ backend/lambdas/v1-services-ai/src/constants.py
@@ -10,5 +10,5 @@ AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", "us-east-1")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME", f"innovafam-{ENV}")
 AWS_MODEL_ARN = os.getenv(
     "AWS_MODEL_ARN",
-    f"arn:aws:bedrock:{AWS_REGION_NAME}:683953625718:inference-profile/global.anthropic.claude-haiku-4-5-20251001-v1:0",
+    f"arn:aws:bedrock:{AWS_REGION_NAME}:376218549266:inference-profile/global.anthropic.claude-haiku-4-5-20251001-v1:0",
 )
